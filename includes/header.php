2bc8f786f8d07924a9c640e198ddddc22fd957b4
Nathan GNANKADJA <gnankadjanathan@gmail.com>
2023-08-26 17:56:29 +0100

    Add header file

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Real Estate Platforme</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <div><img src="" alt="Logo du site"></div>
+    <nav>
+        <ul>
+            <li>Accueil</li>
+            <li>Appartement</li>
+        </ul>
+    </nav>
+    <div>
+        <a href="">Mon espace propriétaire</a>
+    </div>
+    </header>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,3 @@
-<>
+<?php
+    include("includes/header.php");
+?>
